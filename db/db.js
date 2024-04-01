f1e77fefb156becb9d2a5a5258b736c3770ba963
aaocansey <albertocansey582@yahoo.com>
2024-04-01 13:01:39 +0000

    mongo update

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connect = async() => {
     try {
-        await mongoose.connect('mongodb+srv://albertocansey582:GpejiMq0M201qT6s@cluster0.cpscziy.mongodb.net/live_chat?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://albertocansey582:s0JS3LxnYK8yYsP3@cluster0.cpscziy.mongodb.net/live_chat?retryWrites=true&w=majority&appName=Cluster0')
     } catch (error) {
         console.log(error)
     }
